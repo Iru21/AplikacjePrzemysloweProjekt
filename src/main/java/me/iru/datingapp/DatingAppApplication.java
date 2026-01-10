672473c7bb574e8823b758c13dbd0e7f7dfaa0f6
Mateusz Samsel <m.samsel.113@studms.ug.edu.pl>
2026-01-10 21:45:30 +0100

    Base spring project

---
+++ src/main/java/me/iru/datingapp/DatingAppApplication.java
@@ -0,0 +1,13 @@
+package me.iru.datingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DatingAppApplication.class, args);
+    }
+
+}
